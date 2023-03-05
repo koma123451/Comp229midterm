544404c27b177825f93f26f60b880c4c617b25a6
Bowen Dai <daibowen6111@gmail.com>
2023-03-05 14:44:48 -0500

    myMidterm

---
+++ server/config/db.js
@@ -0,0 +1,11 @@
+/******************************
+ * File: db.js
+ * Author: Bowen Dai
+ * Student ID: 300775723
+ * Web app: Favourite Book List
+ ******************************/
+
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://BowenDai:Aa123456@cluster0.opdr9re.mongodb.net/book_store?retryWrites=true&w=majority"
+};
